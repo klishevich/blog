cba01bfefc4ca8534d8f81d3764b7570320d6b93
klishevich <busation@gmail.com>
2025-01-27 22:04:01 +0300

    p01(step3): Add Material UI table

--- react_d3_chart_state_management/README.md
+++ react_d3_chart_state_management/README.md
@@ -6,12 +6,21 @@ State management is one of the most challenging problems when working with chart
 
 ## Step by step tutorial
 
-### First we create an empty React TypeScript app with WebPack
+### 1. First we create an empty React TypeScript app with WebPack
 
 By adding [these files](https://github.com/klishevich/blog/commit/118b24cea3e1385211be3e5eed32d81599d59a9a) we create a new project.
 After running it with `npm run dev` we get this
 
 > ![New React App Image](img/img01.jpg)
 
-### Let us add a simple D3 chart to the page
+### 2. Let us add a simple D3 chart to the page
 
+After installing `d3` npm module and types `@types/d3` we create a line chart with the point markers ([git diff](https://github.com/klishevich/blog/commit/1c650ab5a8e1474cfce28754be02e6241b9d336b))
+
+> ![D3 line chart with point markers image](img/img02.jpg)
+
+### 3. Now let's add Material UI table to the page
+
+After installing Material UI modules `@mui/icons-material`, `@mui/material": "^6.4.1`, `@emotion/react`, `@emotion/styled` fonts `@fontsource/roboto` and Webpack style loaders `css-loader`, `style-loader` we can add a grid to the page.
+
+> ![Add material UI table](img/img03.jpg)
--- react_d3_chart_state_management/app/src/App.tsx
+++ react_d3_chart_state_management/app/src/App.tsx
@@ -1,4 +1,13 @@
 import * as d3 from 'd3';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 type TProps = {
     xValues: number[];
@@ -36,13 +45,64 @@ function LinePlot({
     );
 }
 
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function BasicTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align='right'>Calories</TableCell>
+                        <TableCell align='right'>Fat&nbsp;(g)</TableCell>
+                        <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
+                        <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component='th' scope='row'>
+                                {row.name}
+                            </TableCell>
+                            <TableCell align='right'>{row.calories}</TableCell>
+                            <TableCell align='right'>{row.fat}</TableCell>
+                            <TableCell align='right'>{row.carbs}</TableCell>
+                            <TableCell align='right'>{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <h1>React D3 Chart State </h1>
+                <Typography variant='h2' gutterBottom>
+                    React D3 Chart State
+                </Typography>
             </header>
-            <LinePlot xValues={[1, 2, 3, 5]} yValues={[3, 8, 8, 4]} />
+            <div>
+                <LinePlot xValues={[1, 2, 3, 5]} yValues={[3, 8, 8, 4]} />
+            </div>
+            <div style={{ maxWidth: 800 }}>
+                <BasicTable />
+                <Button variant='contained'>Material UI Button</Button>
+            </div>
         </div>
     );
 }
