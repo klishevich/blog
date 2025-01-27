1c650ab5a8e1474cfce28754be02e6241b9d336b
klishevich <busation@gmail.com>
2025-01-27 21:35:21 +0300

    p01(step2): Add D3 Line chart

--- react_d3_chart_state_management/README.md
+++ react_d3_chart_state_management/README.md
@@ -4,5 +4,14 @@
 
 State management is one of the most challenging problems when working with charting libraries. Of course, it is easy to create a simple chart, but it gets much more complicated when you need to synchronize the state across the whole app, which contains grids, charts, and UI elements. This React, D3, Material UI app demonstrates how to address the state management problem.
 
-## Step 1. Creating React App with TypeScript
+## Step by step tutorial
+
+### First we create an empty React TypeScript app with WebPack
+
+By adding [these files](https://github.com/klishevich/blog/commit/118b24cea3e1385211be3e5eed32d81599d59a9a) we create a new project.
+After running it with `npm run dev` we get this
+
+> ![New React App Image](img/img01.jpg)
+
+### Let us add a simple D3 chart to the page
 
--- react_d3_chart_state_management/app/src/App.tsx
+++ react_d3_chart_state_management/app/src/App.tsx
@@ -1,10 +1,48 @@
+import * as d3 from 'd3';
+
+type TProps = {
+    xValues: number[];
+    yValues: number[];
+    width?: number;
+    height?: number;
+    marginTop?: number;
+    marginRight?: number;
+    marginBottom?: number;
+    marginLeft?: number;
+};
+
+function LinePlot({
+    xValues,
+    yValues,
+    width = 640,
+    height = 400,
+    marginTop = 20,
+    marginRight = 20,
+    marginBottom = 20,
+    marginLeft = 20,
+}: TProps) {
+    const xScale = d3.scaleLinear([0, 10], [marginLeft, width - marginRight]);
+    const yScale = d3.scaleLinear([0, 10], [height - marginBottom, marginTop]);
+    const line = d3.line((d, i) => xScale(xValues[i]), yScale);
+    return (
+        <svg width={width} height={height}>
+            <path fill='none' stroke='currentColor' strokeWidth='1.5' d={line(yValues)} />
+            <g fill='white' stroke='currentColor' strokeWidth='1.5'>
+                {xValues.map((x, i) => (
+                    <circle key={i} cx={xScale(x)} cy={yScale(yValues[i])} r='2.5' />
+                ))}
+            </g>
+        </svg>
+    );
+}
+
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
                 <h1>React D3 Chart State </h1>
             </header>
-            <div>TEST APP</div>
+            <LinePlot xValues={[1, 2, 3, 5]} yValues={[3, 8, 8, 4]} />
         </div>
     );
 }
